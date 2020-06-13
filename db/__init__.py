3554c5f8386a64e3f2e5cdea9a6fd1fd231cfe79
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-06-13 05:58:02 -0700

    commit

--- db/__init__.py
+++ db/__init__.py
@@ -45,6 +45,13 @@ def tryRemove(sub_list, text):
 	except:
 		...
 
+def tryAdd(sub, chat_id, text):
+	if chat_id not in sub:
+		sub[chat_id] = []
+	if text in sub[chat_id]:
+		return
+	sub[chat_id].appebd(text)
+
 def getSubscriptions(sub):
 	result = set()
 	for chat_id in sub:
@@ -71,10 +78,11 @@ class Subscription(object):
 		if not text:
 			return
 		user_id = getUserId(text, twitterApi)
+
 		if user_id:
-			self.user_sub[chat_id] = self.user_sub.get(chat_id, []) + [user_id]
+			tryAdd(self.user_sub, chat_id, user_id)
 		else:
-			self.key_sub[chat_id] = self.key_sub.get(chat_id, []) + [text]
+			tryAdd(self.key_sub, chat_id, text)
 		self.save()
 
 	def remove(self, chat_id, text, twitterApi):
@@ -90,8 +98,7 @@ class Subscription(object):
 		for user_id in self.user_sub.get(chat_id, []):
 			user = twitterApi.get_user(user_id)
 			if user:
-				result.append('[%s](%s)' % (user.name, str(user.screen_name)))
-
+				result.append('[%s](%s)' % (user.name, 'https://twitter.com/' + str(user.screen_name)))
 		return 'subscriptions: ' + ' '.join(result + self.key_sub.get(chat_id, []))
 
 	def keys(self):
