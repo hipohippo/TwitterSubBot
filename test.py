1cb050975ea6ba5a3a92bf22b7d0863ee7c80093
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-05 07:16:25 -0700

    commit

--- test.py
+++ test.py
@@ -3,9 +3,14 @@
 
 import yaml
 import tweepy
+import twitter_2_album
+import album_sender
+from telegram.ext import Updater
 
 with open('CREDENTIALS') as f:
 	credential = yaml.load(f, Loader=yaml.FullLoader)
+tele = Updater(credential['bot'], use_context=True)  # @twitter_send_bot
+debug_group = tele.bot.get_chat(420074357)
 
 auth = tweepy.OAuthHandler(credential['twitter_consumer_key'], credential['twitter_consumer_secret'])
 auth.set_access_token(credential['twitter_access_token'], credential['twitter_access_secret'])
@@ -15,4 +20,5 @@ twitterApi = tweepy.API(auth)
 # 	print(status.id)
 
 for status in twitterApi.search('女权'):
-	print(status.id)
+	r = twitter_2_album.get(str(status.id))
+	album_sender.send_v2(debug_group, r)
