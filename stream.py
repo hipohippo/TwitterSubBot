9294fb2c3c1f3cca6da8be25ded7dfef909e0d84
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-11 16:20:05 -0700

    auto_commit_db

--- stream.py
+++ stream.py
@@ -59,7 +59,7 @@ def shouldProcess(data, db):
 		return True
 	if matchKey(str(data), db.blacklist.items):
 		return False
-	bar = 10000
+	bar = 100000
 	if matchKey(str(data), db.popularlist.items):
 		bar *= 10
 	if isRich(data):
