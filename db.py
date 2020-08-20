7888a698d1ded083f9a4a12940905e667fe6c823
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-20 12:21:34 -0700

    commit

--- db.py
+++ db.py
@@ -1,105 +1,93 @@
-import os
 import yaml
-from telegram_util import commitRepo
+from telegram_util import commitRepo, isInt, matchKey
 import plain_db
+from common import bot, twitterApi
 
-def getUserId(text, twitterApi):
+def getUserId(text):
 	try:
-		screenname = text.strip('/').split('/')[-1]
+		screenname = text.split('?')[0].strip('@').strip('/').split('/')[-1]
 		user = twitterApi.get_user(screenname)
 		return user.id
 	except:
 		...
 
-def tryRemove(sub_list, text):
-	try:
-		sub_list.remove(text)
-	except:
-		...
-
-def tryAdd(sub, chat_id, text):
-	if chat_id not in sub:
-		sub[chat_id] = []
-	if text in sub[chat_id]:
-		return
-	sub[chat_id].append(text)
-
-def getSubscriptions(sub):
-	result = set()
-	for chat_id in sub:
-		for item in sub.get(chat_id, []):
-			result.add(item)
-	return list(result)
-
-def getChannels(bot, sub, text):
-	for chat_id in sub:
-		if text in sub.get(chat_id, []):
-			try:
-				channel = bot.get_chat(chat_id)
-				yield channel
-			except:
-				...
-
-def isInt(text):
-	try:
-		int(text)
-		return True
-	except:
-		return False
+def getMatches(text):
+	if not text:
+		return []
+	if matchKey(text, ['/', '@']) or isInt(text):
+		user_id = getUserId(text)
+		if user_id and isInt(text):
+			return [user_id]
+		if user_id:
+			return [user_id, text]
+	return [text]
 
 class Subscription(object):
 	def __init__(self):
 		with open('db/subscription') as f:
 			self._db = yaml.load(f, Loader=yaml.FullLoader)
 
-	def add(self, chat_id, text, twitterApi):
-		if not text:
-			return
-		user_id = None
-		if '/' in text or isInt(text):
-			user_id = getUserId(text, twitterApi)
-		if user_id:
-			tryAdd(self.user_sub, chat_id, user_id)
-		else:
-			tryAdd(self.key_sub, chat_id, text)
+	def add(self, chat_id, text):
+		matches = getMatches(text)
+		if not matches:
+			return False
+		text = matches[0]
+		self._db[chat_id] = self._db.get(chat_id, [])
+		if text in self._db[chat_id]:
+			return False
+		self._db[chat_id].append(text)
 		self.save()
+		return True
 
-	def remove(self, chat_id, text, twitterApi):
-		user_id = getUserId(text, twitterApi)
-		if user_id:
-			tryRemove(self.user_sub.get(chat_id), user_id)
-		tryRemove(self.key_sub.get(chat_id), text)
-		self.save()
+	def remove(self, chat_id, text):
+		matches = getMatches(text)
+		for match in matches:
+			if match in self._db.get(chat_id, []):
+				self._db[chat_id].remove(match)
+				self.save()
+				return True
+		return False
 
-	def get(self, chat_id, twitterApi):
+	def getSubscription(self, chat_id):
 		result = []
-		for user_id in self.user_sub.get(chat_id, []):
-			user = twitterApi.get_user(user_id)
+		for text in self._db.get(chat_id, []):
+			user = twitterApi.get_user(text)
 			if user:
-				result.append('[%s](%s)' % (user.name, 'https://twitter.com/' + str(user.screen_name)))
-		return 'subscriptions: ' + ' '.join(result + self.key_sub.get(chat_id, []))
+				item = '[%s](%s)' % (user.name, 
+					'https://twitter.com/' + str(user.screen_name))
+				result = [item] + result
+			else:
+				result.append(text)
+		return 'subscriptions: ' + ' '.join(result)
 
-	def keys(self):
-		return getSubscriptions(self.key_sub)
-
-	def users(self):
-		return [str(x) for x in getSubscriptions(self.user_sub)]
-
-	def channelsForUser(self, bot, user_id):
-		return getChannels(bot, self.user_sub, user_id)
+	def getChats(self, text):
+		for chat_id in self._db:
+			if text in self._db[chat_id]: # verified, this works
+				try:
+					yield bot.get_chat(chat_id)
+				except:
+					...
 
 	def save(self):
-		with open('db/user_sub', 'w') as f:
-			f.write(yaml.dump(self.user_sub, sort_keys=True, indent=2, allow_unicode=True))
-		with open('db/key_sub', 'w') as f:
-			f.write(yaml.dump(self.key_sub, sort_keys=True, indent=2, allow_unicode=True))
-		commitRepo(delay_minute=0)
+		with open('db/subscription', 'w') as f:
+			f.write(yaml.dump(self._db, sort_keys=True, indent=2, allow_unicode=True))
+		# commitRepo(delay_minute=0) # testing
+
+def hasPermission(chat_id):
+	try:
+		r = bot.send_message(chat_id, 'test')
+		r.delete()
+		return True
+	except:
+		return False
 
 def batchAdd(old_dict):
-	for channel in old_dict:
-		items = old_dict.get(channel)
+	for chat_id in old_dict:
+		if not hasPermission(chat_id):
+			continue
+		items = old_dict.get(chat_id)
 		for item in items:
-			subscription.add(channel, item)
+			subscription.add(chat_id, item)
 
 def migrate():
 	with open('db/user_sub') as f:
